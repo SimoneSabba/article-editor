bf19cb9f3ad76829a0584d4b3483346a196bcd21
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-10-12 17:03:14 +0100

    added index with app controller

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var myApp = angular.module('app', []);
+
+myApp.controller(
+	'AppCtrl', ['$scope', 'productsService', 'blockService', 
+		function($scope, productsService, blockService) {
+			
+	}]);
