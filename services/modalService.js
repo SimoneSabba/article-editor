b161456948fadd9e32b772df45d7650c3e6a8794
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-10-13 10:58:18 +0100

    added resolve to modal

--- services/modalService.js
+++ services/modalService.js
@@ -1,17 +1,25 @@
-myApp.service('modalService', function($uibModal){
+myApp.service('modalService', function($uibModal, blockService, productsService){
 	
 	var options = {
 		templateUrl: '../components/modal/template.html',
 		controller: 'ModalCtrl',
+		resolve: {}
 	};
 
-	this.open = function(type) {
-		options.resolve = {
-			type: function() {
-				return type;
-			}
+	function addResolveInfo() {
+		options.resolve.info = function() {
+			return info;
 		}
+	}
 
+	function addResolveType(type) {
+		options.resolve.type = function() {
+			return type;
+		}
+	}
+
+	this.open = function(type) {
+		addResolveType(type);
 		$uibModal.open(options);
 	}
 	return this;
